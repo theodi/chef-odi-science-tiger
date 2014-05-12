e52aefe8baef8ebebcfda24ce05a1e71b826c2cb
pikesley <github@orgraphone.org>
2014-05-12 11:59:30 +0100

    Such working

---
+++ Berksfile
@@ -0,0 +1,5 @@
+source "http://api.berkshelf.com"
+
+metadata
+
+cookbook 'chromium', github: 'klyall/cookbook-chromium'
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'odi-science-tiger'
+maintainer       'The Open Data Institute'
+maintainer_email 'tech@theodi.org'
+license          'MIT'
+description      'Sets up a RasPi for an office display'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.4.1'
+
+depends 'chromium'
