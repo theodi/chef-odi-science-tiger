37e335f99fb8b784e022550c4d43c6cf5f160606
pikesley <github@orgraphone.org>
2014-05-12 17:01:14 +0100

    disable screensaver

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,13 +4,29 @@ include_recipe 'chromium'
 
 template '/home/pi/runchromium' do
   source 'runchromium.erb'
+  mode '0755'
+  owner 'pi'
 end
 
 directory '/home/pi/.config/lxsession/LXDE' do
   action :create
   recursive true
+  owner 'pi'
 end
 
 template '/home/pi/.config/lxsession/LXDE/autostart' do
   source 'autostart.erb'
+  owner 'pi'
+end
+
+directory '/etc/xdg/lxsession/LXDE' do
+  action :create
+  recursive true
+  owner 'root'
+end
+
+file '/etc/xdg/lxsession/LXDE/autostart' do
+  action :create
+  owner 'root'
+  content '# this file has been blanked out by chef'
 end
--- test/integration/server/serverspec/chromium_spec.rb
+++ test/integration/server/serverspec/chromium_spec.rb
@@ -5,15 +5,22 @@ describe package 'chromium' do
 end
 
 describe user 'pi' do
-  it { should be_exist }
+  it { should exist }
 end
 
 describe file '/home/pi/runchromium' do
   it { should be_file }
+  it { should be_executable }
+  it { should be_owned_by 'pi'}
   its(:content) { should match '/usr/bin/chromium --kiosk --incognito' }
 end
 
 describe file '/home/pi/.config/lxsession/LXDE/autostart' do
   it { should be_file }
   its(:content) { should match '@~/runchromium' }
+end
+
+describe file '/etc/xdg/lxsession/LXDE/autostart' do
+  it { should be_file }
+  its(:content) { should_not match /xscreensaver/ }
 end
